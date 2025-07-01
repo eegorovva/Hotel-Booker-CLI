8b33854d190457dc5743fc0f6d70d76d372c0daf
eegorovva <gelya.egorova68@gmail.com>
2025-07-01 18:00:18 +0300

    create show_free_rooms

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from db import *
 
-def add_room(id, type, price):
+def add_room(id, type, price): #функция для админа
     connection = get_connection()
 
     with connection.cursor() as curs:
@@ -12,7 +12,6 @@ def add_room(id, type, price):
     connection.close()
 
 
-
 def show_all_rooms():
     connection = get_connection()
 
@@ -20,15 +19,37 @@ def show_all_rooms():
         curs.execute(
             "SELECT room_id, room_type, price FROM rooms"
         )
-        rows = curs.fetchall()
+        all_rooms = curs.fetchall()
 
     connection.close()
 
-    return rows
+    return all_rooms
+
+def show_free_rooms(date_from, date_to):
+    connection = get_connection()
+
+    with connection.cursor() as curs:
+        curs.execute(
+            """SELECT room_id, room_type, price
+            FROM rooms r
+            where not exists(
+                SELECT 1
+                FROM bookings b
+                WHERE room_id = r.room_id
+                AND daterange(date_from, date_to) && daterange(%s, %s)
+            )
+            ORDER BY price
+            """, (date_from, date_to)
+        )
+
+        free_rooms = curs.fetchall()
+
+        connection.close()
+
+        return free_rooms
 
 
-
-rooms = show_all_rooms()
+rooms = show_free_rooms("10.12.2025","12.12.2025")
 
 for room_id, room_type, price in rooms:
     text = f"Room number: {room_id}  | type: {room_type}  |  price: {price} $"
